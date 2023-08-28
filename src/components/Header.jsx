349a5897b1e84bf59e44a481131b3845de22f2a8
jeeonglee1 <jeeonglee1@gmail.com>
2023-08-28 15:56:29 -0300

    feat: add initial header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <img src="./images/logo.svg" alt="Logo da Nike" />
+        <a href="/">Women</a>
+        <a href="/">Men</a>
+        <a href="/">Kids</a>
+        <a href="/">Collections</a>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
